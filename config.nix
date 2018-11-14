54242c328df374582610a60216b754bd6a3272a2
Michael Hudson <huddo121@gmail.com>
2018-11-14 19:56:29 +1100

    Update HIE to 0.3.0.0 and GHC to 8.4.4

--- config.nix
+++ config.nix
@@ -1,5 +1,5 @@
 rec {
-  ghcVersion = "ghc843";
+  ghcVersion = "ghc844";
   nixpkgs = import (fetchTarball https://nixos.org/channels/nixos-18.09/nixexprs.tar.xz) {
     config = {
       packageOverrides = pkgs: rec {
@@ -13,5 +13,5 @@ rec {
       };
     };
   };
-  hie = import (fetchTarball https://github.com/domenkozar/hie-nix/tarball/96af698f0cfefdb4c3375fc199374856b88978dc) { };
+  hie = import (fetchTarball https://github.com/domenkozar/hie-nix/tarball/157d1bcde9d4fb87ec3b9fb23b69513fb3f62229) { };
 }
